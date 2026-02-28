1156331a8a10197ed968505ff805afa99f0a0414
aungtuntun-9823 <e1509823@u.nus.edu>
2026-02-28 17:16:20 +0800

    Secured practices

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'http://localhost:30081/auth', // Update with your authorization server URL
-  redirectUri: window.location.origin,
+  redirectUri: globalThis.location.origin,
   clientId: 'client',
   responseType: 'code',
   scope: 'openid',
