f2e867e0ee297b5cb8e0f5c60b42e1420a9fd502
William Siling <e1503302@u.nus.edu>
2026-04-11 15:48:02 +0800

    build local using localk8s value

--- build-and-push-docker-image-local.sh
+++ build-and-push-docker-image-local.sh
@@ -6,7 +6,8 @@ TAG=v0.1.0
 LOCAL_REPO=localhost:5000
 
 # Build the Docker image
-docker build -t $IMAGE_NAME:$TAG .
+#docker build --build-arg -t $IMAGE_NAME:$TAG .
+docker build --build-arg BUILD_CONFIG=localk8s -t $IMAGE_NAME:$TAG .
 
 # Push the image to the local repository
 docker push $LOCAL_REPO/$IMAGE_NAME:$TAG
